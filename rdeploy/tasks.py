a9e203f2ad2f06f20a920fb704bcfb3e14fb35a5
Michail Brynard <mbrynard@gmail.com>
2021-08-12 12:57:59 +0200

    Remove config dict from management command as it is not needed

--- rdeploy/tasks.py
+++ rdeploy/tasks.py
@@ -506,7 +506,7 @@ def shell(ctx, config):
     set_context(ctx, config)
     settings_dict = get_settings()
     config_dict = settings_dict['configs'][config]
-    management_cmd = build_management_cmd(config_dict, config, "/bin/bash")
+    management_cmd = build_management_cmd(config_dict, "/bin/bash")
     ctx.run(management_cmd, pty=True, warn=False, echo=True)
 
 
@@ -516,7 +516,7 @@ def manage(ctx, config, cmd, tag=None):
     set_context(ctx, config)
     settings_dict = get_settings()
     config_dict = settings_dict['configs'][config]
-    management_cmd = build_management_cmd(config_dict, config, f'python manage.py {cmd}', tag)
+    management_cmd = build_management_cmd(config_dict, f'python manage.py {cmd}', tag)
     ctx.run(management_cmd, pty=True, warn=False, echo=True)
 
 
